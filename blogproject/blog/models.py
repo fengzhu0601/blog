96f3a9dc2cec0ceaede427a981e527a14479f0f2
fengzhu <fengzhu@blizzmi.com>
2018-05-04 17:44:16 +0800

    使用模板

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.six import python_2_unicode_compatible
 
 
 # Create your models here.
@@ -21,6 +23,7 @@ class Tag(models.Model):
 
 
 # 文章
+@python_2_unicode_compatible
 class Post(models.Model):
     # 文章标题
     title = models.CharField(max_length=70)
@@ -45,3 +48,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -6,8 +6,10 @@ __date__ = '18-5-4 下午2:40'
 from django.conf.urls import url
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
 
 
