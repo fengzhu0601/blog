2a8ef2dc36ce523aa3ff5fed625bdd2f5255f059
fengzhu <fengzhu@blizzmi.com>
2018-05-04 15:29:03 +0800

    网上下载的网站模板

--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -8,10 +8,16 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 # 标签
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 # 文章
@@ -36,3 +42,6 @@ class Post(models.Model):
 
     # 文章作者
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
